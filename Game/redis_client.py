0c528c526e010f1118e0dffd39cddf800863b3a6
Ayman <theunsungheroofthemonth@gmail.com>
2025-08-12 11:17:24 +0800

    redis pass

--- Game/redis_client.py
+++ Game/redis_client.py
@@ -1,4 +1,4 @@
 import redis.asyncio as redis
 
-pool = redis.ConnectionPool.from_url("rediss://phantom-chess.redis.cache.windows.net:6380/0")
+pool = redis.ConnectionPool.from_url("rediss://{6Xuw0ogshvbmjktdNPwmLmKdR4nGHqVq9AzCaGfM8WE=}@phantom-chess.redis.cache.windows.net:6380/0")
 redis_client = redis.Redis(connection_pool=pool)
